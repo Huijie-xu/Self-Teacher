a471ac9413fd5394bbe034a1b894cb9053e5feb5
huijiexu <huijiexu@microsoft.com>
2025-01-18 21:39:35 +0800

    add multi options and left-right layout

--- main.py
+++ main.py
@@ -17,14 +17,24 @@ st.set_page_config(
 )
 
 with st.form("my_form"):
-    question = st.text_input('请输入你的问题')
+    with st.sidebar:
+        languageArr = ['English', '中文']
+        langoption = st.selectbox('Language', languageArr)
 
-    submitted = st.form_submit_button("Submit")
+        classArr = ['数学','语文','英语']
+        classoption = st.selectbox('科目', classArr)
+
+        gradeArr = ['小学','初中','高中']
+        gradeoption = st.selectbox('学级', gradeArr)
+
+        question = st.text_input('请输入你的问题')
+
+        submitted = st.form_submit_button("Submit")
 
     if submitted and question:
         with st.spinner('加载解读中，请稍等 ......'):
 
-            system_prompt = """你是一个专业的小学数学老师，你收到了学生提出的数学问题，需要很专业的从理论背景，到推理过程，到最后结果的计算详尽的为学生解答数学问题，力争让每个人都能通过你的解答完全学会题目的解答原理。不仅仅是提供一个简单的计算结果或者答案。"""
+            system_prompt = f"""你是一个专业的{gradeoption}{classArr}老师，你收到了学生提出的{classArr}问题，需要很专业的从理论背景，到推理过程，到最后结果的计算详尽的为学生解答数学问题，力争让每个人都能通过你的解答完全学会题目的解答原理。不仅仅是提供一个简单的计算结果或者答案。最后给出的答案，有语言的限制，请用{langoption}回答"""
 
             response = openai.ChatCompletion.create(
                 engine="gpt4",
