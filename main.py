6c407be4eded3a07f743f122c54297b945a8c45f
huijiexu <huijiexu@microsoft.com>
2025-04-06 20:43:26 +0800

    Add responsive selection and fix prompt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import openai
 import os
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,25 +17,50 @@ st.set_page_config(
     layout="centered",
 )
 
+with open('options.json', encoding='utf-8') as file:
+  file_contents = file.read()
+options_content = json.loads(file_contents)
+
+
+if 'language' not in st.session_state:
+    st.session_state['language'] = 'English'
+
+langoption = st.sidebar.selectbox('Language/语言', ['English', '中文'], key='language')
+
+# Update content based on language
+if langoption == 'English':
+    content = options_content["EN"]
+else:
+    content = options_content["CN"]
+
+
 with st.form("my_form"):
     with st.sidebar:
         languageArr = ['English', '中文']
-        langoption = st.selectbox('Language', languageArr)
 
-        classArr = ['数学','语文','英语']
-        classoption = st.selectbox('科目', classArr)
+        st.session_state.update({
+            'class_array': content['class'],
+            'grade_array': content['grade'],
+            'class_title': content['class_title'],
+            'grade_title': content['grade_title'],
+            'input_text': content['input_text'],
+            'sub_text': content['sub_text']
+        })
 
-        gradeArr = ['小学','初中','高中']
-        gradeoption = st.selectbox('学级', gradeArr)
+        classArr = st.session_state['class_array']
+        classoption = st.selectbox(st.session_state['class_title'], classArr)
 
-        question = st.text_input('请输入你的问题')
+        gradeArr = st.session_state['grade_array']
+        gradeoption = st.selectbox(st.session_state['grade_title'], gradeArr)
 
-        submitted = st.form_submit_button("Submit")
+        question = st.text_input(st.session_state['input_text'])
+
+        submitted = st.form_submit_button(st.session_state['sub_text'])
 
     if submitted and question:
         with st.spinner('加载解读中，请稍等 ......'):
 
-            system_prompt = f"""你是一个专业的{gradeoption}{classArr}老师，你收到了学生提出的{classArr}问题，需要很专业的从理论背景，到推理过程，到最后结果的计算详尽的为学生解答数学问题，力争让每个人都能通过你的解答完全学会题目的解答原理。不仅仅是提供一个简单的计算结果或者答案。最后给出的答案，有语言的限制，请用{langoption}回答"""
+            system_prompt = f"""你是一个专业的{gradeoption}{classoption}老师，你收到了学生提出的{question}问题，需要很专业的从理论背景，到推理过程，到最后结果的计算详尽的为学生解答数学问题，力争让每个人都能通过你的解答完全学会题目的解答原理。不仅仅是提供一个简单的计算结果或者答案。最后给出的答案，有语言的限制，请用{langoption}回答，请注意减少你对如何给他人教学的建议，只提供问题的详细解答就行。"""
 
             response = openai.ChatCompletion.create(
                 engine="gpt4",
